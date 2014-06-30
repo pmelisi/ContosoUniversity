1410a6ab3211bf118bc21ad78fd5fe65f6067ec7
pmelisi <pmelisi@gmail.com>
2014-06-30 15:13:44 +1000

    Part 5 - 9 Completed

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace ContosoUniversity.Models
 {
     public enum Grade
@@ -11,6 +12,7 @@ namespace ContosoUniversity.Models
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public virtual Course Course { get; set; }
